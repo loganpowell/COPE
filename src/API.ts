a9db54154bd3b28111e0f4464c58bae0077b64e5
Logan Powell <loganp@tepper.cmu.edu>
2021-07-28 10:20:55 -0400

    updates to schema

--- src/API.ts
+++ src/API.ts
@@ -248,6 +248,7 @@ export enum NodeType {
   A_PAGE = "A_PAGE",
   A_APPLICATION = "A_APPLICATION",
   A_GEM = "A_GEM",
+  A_COURSE = "A_COURSE",
   S_ACS = "S_ACS",
   S_DECENNIAL = "S_DECENNIAL",
   S_CBP = "S_CBP",
@@ -255,6 +256,7 @@ export enum NodeType {
   V_2000 = "V_2000",
   V_2010 = "V_2010",
   V_2020 = "V_2020",
+  C_COURSES = "C_COURSES",
   C_SERIES = "C_SERIES",
   C_LIST = "C_LIST",
 }
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * as mutations from "./graphql/mutations"
+export * as queries from "./graphql/queries"
+export * as subscriptions from "./graphql/subscriptions"
+export * as API from "./API"
